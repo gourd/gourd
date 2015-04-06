032901667fc52fa8f418b8de857834efabdf68ff
Koala Yeung <koalay@gmail.com>
2015-04-07 03:29:35 +0800

    Basic blocks of code parser complete
    Written re-usable pipelines for parsing Go codes. Especially
    parsing structs, struct fields and tags.

---
+++ astutils.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"go/ast"
+)
+
+// representation of a general type spec
+type genTypeSpec struct {
+	Pkg    string
+	Name   string
+	Type   string
+	Fields []fieldSpec
+}
+
+// representation of a general struct field
+type fieldSpec struct {
+	Name string
+	Type string
+	Tag  string
+}
+
+// channel the slice of ast.Decl out for filtering
+func chanDecls(decls []ast.Decl) <-chan ast.Decl {
+	cout := make(chan ast.Decl)
+	go func(decls []ast.Decl, cout chan ast.Decl) {
+		defer close(cout)
+		for _, decl := range decls {
+			cout <- decl
+		}
+	}(decls, cout)
+	return cout
+}
+
+// filter only GetDecl
+func filterGenDecl(cin <-chan ast.Decl) <-chan *ast.GenDecl {
+	cout := make(chan *ast.GenDecl)
+	go func(cin <-chan ast.Decl, cout chan *ast.GenDecl) {
+		defer close(cout)
+		for decl := range cin {
+			if genDecl, ok := decl.(*ast.GenDecl); ok {
+				cout <- genDecl
+			}
+		}
+	}(cin, cout)
+	return cout
+}
+
+// filter only TypeSpec
+func filterTypeSpec(cin <-chan *ast.GenDecl) <-chan *ast.TypeSpec {
+	cout := make(chan *ast.TypeSpec)
+	go func(cin <-chan *ast.GenDecl, cout chan *ast.TypeSpec) {
+		defer close(cout)
+		for decl := range cin {
+			// Note: may further break up this into
+			//       another pipeline function
+			for _, spec := range decl.Specs {
+				if typeSpec, ok := spec.(*ast.TypeSpec); ok {
+					cout <- typeSpec
+				}
+			}
+		}
+	}(cin, cout)
+	return cout
+}
+
+// filter only StructType
+func filterStructType(cin <-chan *ast.TypeSpec) <-chan *ast.StructType {
+	cout := make(chan *ast.StructType)
+	go func(cin <-chan *ast.TypeSpec, cout chan *ast.StructType) {
+		defer close(cout)
+		for spec := range cin {
+			if structType, ok := spec.Type.(*ast.StructType); ok {
+				cout <- structType
+			}
+		}
+	}(cin, cout)
+	return cout
+}
+
+// parse a type spec into genTypeSpec
+func parseTypeSpec(typeSpec *ast.TypeSpec) (spec genTypeSpec) {
+
+	// name of the type
+	if typeSpec.Name != nil {
+		spec.Name = typeSpec.Name.Name
+	}
+
+	// if this is a struct
+	if structType, ok := typeSpec.Type.(*ast.StructType); ok {
+		spec.Type = "struct"
+
+		// see FieldList, Field, Tag in package "os/ast"
+		for _, f := range structType.Fields.List {
+			fspec := fieldSpec{}
+			if len(f.Names) > 0 {
+				fspec.Name = f.Names[0].Name
+			}
+			if f.Tag != nil {
+				// remove the "`" from beginning and end
+				fspec.Tag = f.Tag.Value[1 : len(f.Tag.Value)-1]
+			}
+			if f.Type != nil {
+				if id, ok := f.Type.(*ast.Ident); ok {
+					fspec.Type = id.Name
+				}
+			}
+			spec.Fields = append(spec.Fields, fspec)
+		}
+	}
+
+	return
+}
--- gen_service.go
+++ gen_service.go
@@ -3,11 +3,14 @@ package main
 import (
 	"fmt"
 	"github.com/codegangsta/cli"
+	"go/parser"
+	"go/token"
 	"os"
 	"strings"
 )
 
 func init() {
+	// declare sub-command "gourd gen service"
 	appendCmds("gen", cli.Command{
 		Name:    "service",
 		Aliases: []string{"s"},
@@ -28,6 +31,7 @@ func init() {
 	})
 }
 
+// generate the service go file
 func genService(c *cli.Context) {
 
 	// files to parse
@@ -45,16 +49,50 @@ func genService(c *cli.Context) {
 		fmt.Println("Please provide the target type(s)")
 		os.Exit(1)
 	}
-	t := c.String("type")
+	tn := c.String("type")
 
 	// output file
 	var o string
 	if c.String("output") == "" {
-		o = strings.ToLower(t) + "_service.go"
+		o = strings.ToLower(tn) + "_service.go"
 	} else {
 		o = c.String("output")
 	}
 
+	// read type of type name from given file(s)
+	t := readTypeFile(fns[0], tn)
+
 	// dummy output
-	fmt.Printf("t: %s, o: %s, fns: %s\n", t, o, fns)
+	fmt.Printf("t: %#v ==> output: %s\n", t, o)
+}
+
+// read typeSpec from files
+func readTypeFile(inputPath string, tn string) (spec genTypeSpec) {
+	fset := token.NewFileSet()
+
+	// TODO: get to know what inputPath can be (e.g. dir?)
+	f, err := parser.ParseFile(fset, inputPath, nil, parser.ParseComments)
+	if err != nil {
+		panic(err)
+	}
+
+	// read package name
+	var pkg string
+	if f.Name == nil {
+		fmt.Println("Error. Unknown package name. Exit.")
+		os.Exit(1)
+	} else {
+		pkg = f.Name.Name
+	}
+
+	// read types name and details
+	for ts := range filterTypeSpec(filterGenDecl(chanDecls(f.Decls))) {
+		pts := parseTypeSpec(ts)
+		if pts.Name == tn {
+			spec = pts
+			break
+		}
+	}
+	spec.Pkg = pkg
+	return
 }
