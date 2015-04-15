5849049c6c8bf9b489487de385fa28e393aded38
Koala Yeung <koalay@gmail.com>
2015-04-15 22:52:56 +0800

    Parse with the new goparser library
    Moved utils_parser.go and improved.
    Made a new goparser library with that.

--- utils_parse.go
+++ utils_parse.go
@@ -2,145 +2,13 @@ package main
 
 import (
 	"fmt"
-	"go/ast"
+	"github.com/gourd/goparser"
 	"go/parser"
 	"go/token"
 )
 
-// representation of a general type spec
-type pTypeSpec struct {
-	Name    string
-	Type    string
-	Fields  []pFieldSpec
-	Doc     string
-	Comment string
-}
-
-// representation of a general struct field
-type pFieldSpec struct {
-	Name    string
-	Type    string
-	Tag     string
-	Doc     string
-	Comment string
-}
-
-// channel the slice of ast.Decl out for filtering
-func chanDecls(decls []ast.Decl) <-chan ast.Decl {
-	cout := make(chan ast.Decl)
-	go func(decls []ast.Decl, cout chan ast.Decl) {
-		defer close(cout)
-		for _, decl := range decls {
-			cout <- decl
-		}
-	}(decls, cout)
-	return cout
-}
-
-// filter only GetDecl
-func filterGenDecl(cin <-chan ast.Decl) <-chan *ast.GenDecl {
-	cout := make(chan *ast.GenDecl)
-	go func(cin <-chan ast.Decl, cout chan *ast.GenDecl) {
-		defer close(cout)
-		for decl := range cin {
-			if genDecl, ok := decl.(*ast.GenDecl); ok {
-				cout <- genDecl
-			}
-		}
-	}(cin, cout)
-	return cout
-}
-
-// filter only TypeSpec
-func filterTypeSpec(cin <-chan *ast.GenDecl) <-chan *ast.TypeSpec {
-	cout := make(chan *ast.TypeSpec)
-	go func(cin <-chan *ast.GenDecl, cout chan *ast.TypeSpec) {
-		defer close(cout)
-		for decl := range cin {
-			// Note: may further break up this into
-			//       another pipeline function
-			for _, spec := range decl.Specs {
-				if typeSpec, ok := spec.(*ast.TypeSpec); ok {
-					if typeSpec.Doc == nil {
-						// Don't know why the typeSpec doesn't carry
-						// the doc of the parent declaration.
-						// Pass it through.
-						typeSpec.Doc = decl.Doc
-					}
-					cout <- typeSpec
-				}
-			}
-		}
-	}(cin, cout)
-	return cout
-}
-
-// filter only StructType
-func filterStructType(cin <-chan *ast.TypeSpec) <-chan *ast.StructType {
-	cout := make(chan *ast.StructType)
-	go func(cin <-chan *ast.TypeSpec, cout chan *ast.StructType) {
-		defer close(cout)
-		for spec := range cin {
-			if structType, ok := spec.Type.(*ast.StructType); ok {
-				cout <- structType
-			}
-		}
-	}(cin, cout)
-	return cout
-}
-
-// parse a type spec into pTypeSpec
-func parseTypeSpec(typeSpec *ast.TypeSpec) (spec pTypeSpec) {
-
-	// name of the type
-	if typeSpec.Name != nil {
-		spec.Name = typeSpec.Name.Name
-	}
-
-	// read comment and docs
-	spec.Doc = commentGroupStr(typeSpec.Doc)
-	spec.Comment = commentGroupStr(typeSpec.Comment)
-
-	// if this is a struct
-	if structType, ok := typeSpec.Type.(*ast.StructType); ok {
-		spec.Type = "struct"
-
-		// see FieldList, Field, Tag in package "os/ast"
-		for _, f := range structType.Fields.List {
-			fspec := pFieldSpec{}
-			if len(f.Names) > 0 {
-				fspec.Name = f.Names[0].Name
-			}
-			if f.Tag != nil {
-				// remove the "`" from beginning and end
-				fspec.Tag = f.Tag.Value[1 : len(f.Tag.Value)-1]
-			}
-			if f.Type != nil {
-				if id, ok := f.Type.(*ast.Ident); ok {
-					fspec.Type = id.Name
-				}
-			}
-			fspec.Doc = commentGroupStr(f.Doc)
-			fspec.Comment = commentGroupStr(f.Comment)
-			spec.Fields = append(spec.Fields, fspec)
-		}
-	}
-
-	return
-}
-
-// test if a string exists in a string slice
-func stringInSlice(a string, list []string) bool {
-	for _, b := range list {
-		if b == a {
-			return true
-		}
-	}
-	return false
-}
-
-// read pTypeSpec of given type names from a file
-func readTypeFile(inputPath string, tns []string) (pkg string, specs []pTypeSpec, err error) {
+// read TypeSpec of given type names from a file
+func readTypeFile(inputPath string, tns []string) (pkg string, specs []*goparser.TypeSpec, err error) {
 	fset := token.NewFileSet()
 
 	// inputPath can only be filename
@@ -157,8 +25,7 @@ func readTypeFile(inputPath string, tns []string) (pkg string, specs []pTypeSpec
 	}
 
 	// read types name and details
-	for ts := range filterTypeSpec(filterGenDecl(chanDecls(f.Decls))) {
-		pts := parseTypeSpec(ts)
+	for pts := range goparser.DeclsToTypes(f.Decls) {
 		if stringInSlice(pts.Name, tns) {
 			specs = append(specs, pts)
 		}
@@ -173,22 +40,12 @@ func readTypeFile(inputPath string, tns []string) (pkg string, specs []pTypeSpec
 	return
 }
 
-// read comment group to string
-func commentGroupStr(cmt *ast.CommentGroup) (str string) {
-
-	// test if empty
-	if cmt == nil || cmt.List == nil || len(cmt.List) == 0 {
-		return ""
-	}
-
-	// append all strings
-	for _, c := range cmt.List {
-		if c != nil {
-			str += c.Text
+// test if a string exists in a string slice
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
 		}
 	}
-
-	// TODO: remove comment signs "//", "/*", "*/" and etc
-
-	return
+	return false
 }
