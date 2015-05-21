bff8fe96a2ff9388075c6e7f0204e3212354f0c9
Koala Yeung <koalay@gmail.com>
2015-05-22 02:27:31 +0800

    Added dummy generators for client and user (exp2)

--- _examples/2_server/osin_client_test.go
+++ _examples/2_server/osin_client_test.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"github.com/RangelReale/osin"
+	"math/rand"
 	"testing"
 )
+
+func dummyNewClient(redirectUri string) *Client {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+	randSeq := func(n int) string {
+		b := make([]rune, n)
+		for i := range b {
+			b[i] = letters[rand.Intn(len(letters))]
+		}
+		return string(b)
+	}
+
+	return &Client{
+		StrId:       randSeq(10),
+		Secret:      randSeq(10),
+		RedirectUri: redirectUri,
+		UserId:      0,
+	}
+}
 
 func TestClient(t *testing.T) {
 	var c osin.Client = &Client{}
--- _examples/2_server/user_test.go
+++ _examples/2_server/user_test.go
@@ -1,8 +1,26 @@
 package main
 
 import (
+	"math/rand"
 	"testing"
 )
+
+func dummyNewUser() *User {
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+	randSeq := func(n int) string {
+		b := make([]rune, n)
+		for i := range b {
+			b[i] = letters[rand.Intn(len(letters))]
+		}
+		return string(b)
+	}
+
+	return &User{
+		Username: randSeq(10),
+		Password: randSeq(8),
+	}
+}
 
 func TestUser(t *testing.T) {
 	var u OAuth2User = &User{}
